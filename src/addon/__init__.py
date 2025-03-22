caf4129e7443adfe99a31ed3a591d2e978e6e085
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2025-03-22 19:34:30 -0400

    chore: add keyboard shortcuts to count notes marked for review and open the standalone editor

--- src/addon/__init__.py
+++ src/addon/__init__.py
@@ -1,4 +1,4 @@
-from aqt import QAction, gui_hooks, mw, qconnect
+from aqt import QAction, QKeySequence, gui_hooks, mw, qconnect
 
 from .note_counter import (
     display_notes_marked_for_review_count,
@@ -10,11 +10,13 @@ from .note_formatter import add_custom_button
 def setup_addon():
     # Add option in "Tools" to count notes that require formatting changes
     action = QAction("Count notes marked for review", mw)
+    action.setShortcut(QKeySequence("c"))
     qconnect(action.triggered, display_notes_marked_for_review_count)
     mw.form.menuTools.addAction(action)
 
     # Add option in "Tools" to format notes using AI
     action = QAction("Improve note using AI", mw)
+    action.setShortcut(QKeySequence("r"))
     qconnect(action.triggered, open_standalone_editor)
     mw.form.menuTools.addAction(action)
 
